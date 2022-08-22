1ebca8b35147f2c7d1264707f58e830c0f55742f
Assis Junior <assisjrs@gmail.com>
2022-08-21 22:31:23 -0300

    inclusão do projeto inicial

---
+++ src/main/java/com/assisjrs/sample/dojomigrations/DojoMigrationsApplication.java
@@ -0,0 +1,13 @@
+package com.assisjrs.sample.dojomigrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoMigrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoMigrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assisjrs/sample/dojomigrations/DojoMigrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assisjrs.sample.dojomigrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoMigrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
